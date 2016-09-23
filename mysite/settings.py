13175afb61637cf3abddda46b7383b22fede5be8
iceedemons <dafty_@hotmail.com>
2016-09-23 22:30:13 +0100

    second phase

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'app',
-        'USER': 'michael',
-        'PASSWORD': 'Spudl3tt',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
